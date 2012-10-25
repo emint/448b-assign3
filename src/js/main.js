3eff3473077df3b1cf4406376e7dd797ffb9a9e7
emint <emint@emint-G53SX.(none)>
2012-10-25 14:38:29 -0700

    Preliminary refactoring, moving out graph layout to own method and leveraging it. Switching naming convention for methods.

--- src/js/main.js
+++ src/js/main.js
@@ -13,33 +13,23 @@ var edgeArray = [];
 var nodeToEdgeArray = {};
 
 
-
-
 d3.csv(PEOPLE_PATH, function(csv) {
     peopledata = csv;
-    if ((--filesToLoad) < 1) Initialize();
+    if ((--filesToLoad) < 1) initialize();
 });
 
 d3.csv(PLACES_PATH, function(csv) {
     placesdata = csv;
-    if ((--filesToLoad) < 1) Initialize();
+    if ((--filesToLoad) < 1) initialize();
 });
 
-function Initialize() {
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
-
-    var svg = d3.select("#overview").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    PopulateArrays();
-    DrawOverviewGraph(force, svg);
+function initialize() {
+    populateArrays();
+    drawGraph("#overview", "overview-svg", peopledata, edgeArray)
+        .on("click", drawIndividualGraph);
 }
 
-function PopulateArrays() {
+function populateArrays() {
     for(var i = 0; i < peopledata.length; i++) {
         var rand = Math.random();
         if (rand > .7) {
@@ -54,51 +44,12 @@ function PopulateArrays() {
     }
 }
 
-function DrawOverviewGraph(force, svg) {
-    force
-        .nodes(peopledata)
-        .links(edgeArray)
-        .start();
-
-    var link = svg.selectAll("line.link")
-        .data(edgeArray)
-        .enter().append("line")
-          .attr("class", "link")
-          .style("stroke", "#999")
-          .style("opacity", ".6")
-          .style("stroke-width", 4);
-  
-    var node = svg.selectAll("circle.node")
-        .data(peopledata)
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("r", 5)
-        .on("click", DrawIndividualGraph);
-
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-        node.attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
-    });
-}
-
-function DrawIndividualGraph(currentPerson) {
+function drawIndividualGraph(currentPerson) {
     var currentPersonEdges = nodeToEdgeArray[currentPerson.unique_id];
     if (currentPersonEdges == null) return;
     
-    d3.selectAll(".ind-swg").remove();    
-    var svg = d3.select("#individual").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("class", "ind-swg");
-
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
+    //Remove previous graph if it exists 
+    d3.selectAll(".ind-svg").remove();    
 
     var neighbors = [currentPerson];
     var individualEdgeArray = [];
@@ -108,13 +59,29 @@ function DrawIndividualGraph(currentPerson) {
         individualEdgeArray.push({source: 0, target: i + 1});
     }
 
+    drawGraph("#individual", "ind-svg", neighbors, individualEdgeArray);
+}
+
+// Draws graph with class 'graphClass' into specified div. Returns reference
+// to node object for further custom functionality.
+function drawGraph(divToAddTo, graphClass, nodes, edges) {
+    var svg = d3.select(divToAddTo).append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", graphClass);
+
+    var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .size([width, height]);
+
     force
-        .nodes(neighbors)
-        .links(individualEdgeArray)
+        .nodes(nodes)
+        .links(edges)
         .start();
 
     var link = svg.selectAll("line.link")
-        .data(individualEdgeArray)
+        .data(edges)
         .enter().append("line")
         .attr("class", "link")
         .style("stroke", "#999")
@@ -122,7 +89,7 @@ function DrawIndividualGraph(currentPerson) {
         .style("stroke-width", 4);
 
     var node = svg.selectAll("circle.node")
-        .data(neighbors)
+        .data(nodes)
         .enter().append("circle")
         .attr("class", "node")
         .attr("r", 5);
@@ -134,5 +101,6 @@ function DrawIndividualGraph(currentPerson) {
                 .attr("y2", function(d) { return d.target.y; });
             node.attr("cx", function(d) { return d.x; })
                 .attr("cy", function(d) { return d.y; });
-        });    
+        });
+    return node; 
 }
