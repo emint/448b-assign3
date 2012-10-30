40df1b56c79d14dafc3ae4967352488c0b14e936
emint <emint@emint-G53SX.(none)>
2012-10-30 10:09:25 -0700

    Cliking now shows a bubble chart

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,9 @@ var PLACES_PATH = "../../data/polis_data.csv";
 
 var filesToLoad = 2;
 
-var width = 550,
-    height = 550;
+var width = 650,
+    height = 550,
+    r = 6;
 
 var greatestPolisNumber = 1035;
 
@@ -27,18 +28,55 @@ d3.csv(PLACES_PATH, function(csv) {
 function initialize() {
     populatePolisResidents();
     createEdgesBetweenPlaces();
-    nodes = placesdata.filter(function(d) { 
+    nodes = placesdata.filter(function(d) {
         return d['edges'] != null;
         });
     var graph = drawGraph("#overview", "overview-svg", nodes, edgeArray);
     graph['nodes']
         .attr("id", function(d) { return d['polis_number']; })       
-        .on("click", drawIndividualGraph)
+        .on("click", drawIndividualGraphAsCirclePack)
         .on("mouseover", mouseEnteredNode)
-        .on("mouseout", mouseLeftNode);
+        .attr("r", function(d) {
+            var totalPeopleCount = parseInt(d['people'].length + 4);
+            var bucketed = d3.scale.log().domain([5, 453]).range([5, 20]);
+            return bucketed(totalPeopleCount);
+          });
     graph['edges'].style("stroke-width", function(d) { 
             return d['sharedPeople'].length;
+        }).style("stroke", function(d) {
+            return getColorForEndeavor(getMostRepEndeavor(d.counts));                     
         });
+}
+
+function getMostRepEndeavor(counts) {
+    var endeavor = "";
+    var maxCount = 0;
+    for (var endeavorKey in counts) {
+        if(counts[endeavorKey] > maxCount) {
+           endeavor = endeavorKey;
+           maxCount = counts[endeavorKey];
+        }
+    }
+    return endeavor;
+}
+
+function getColorForEndeavor(endeavor) {
+    switch(endeavor) {
+        case "culture":
+            return "#E4IAIC";
+        case "philosophy":
+            return "#377EB8";
+        case "economy":
+            return "#4DAF4A";
+        case "politics":
+            return "#984EA3";
+        case "military":
+            return "#FF7F00";
+        case "religion":
+            return "#FFFF33";
+        default:
+            return "000000";
+    }
 }
 
 function mouseEnteredNode(currentPlace) {
@@ -49,11 +87,6 @@ function mouseEnteredNode(currentPlace) {
       .attr("dx", 12)
       .attr("dy", ".35em")
       .text(currentPlace['Toponym']);
-  console.log(currentPlace);
-}
-
-function mouseLeftNode(currentPlace) {
-  console.log("Goodbye world.");
 }
 
 function createEdgesBetweenPlaces() {
@@ -63,8 +96,10 @@ function createEdgesBetweenPlaces() {
                 var intersection = arraysIntersect(placesdata[firstPolisNumber]['people'], 
                         placesdata[secondPolisNumber]['people']);
                 if (intersection.length != 0) {
+                    var counts = getEndeavourCounts(intersection);
                     edgeArray.push({source: placesdata[firstPolisNumber], 
-                        target: placesdata[secondPolisNumber], sharedPeople: intersection});
+                        target: placesdata[secondPolisNumber], sharedPeople: intersection,
+                        counts:counts});
                     var currentPlacePolisNumber = placesdata[firstPolisNumber]['polis_number'];
                     if(placesdata[firstPolisNumber]['edges'] == null) {
                         placesdata[firstPolisNumber]['edges'] = [];
@@ -74,6 +109,21 @@ function createEdgesBetweenPlaces() {
             }
         }
     }
+}
+
+
+function getEndeavourCounts(peopleList) {
+  var counts = {culture:0, economy:0, philosophy:0, politics:0, military:0, religion:0};
+  for (var i=0; i<peopleList.length; i++) {
+    var person = peopledata[peopleList[i]];
+    counts['culture'] += parseInt(person['culture']);
+    counts['economy'] += parseInt(person['economy']);
+    counts['philosophy'] += parseInt(person['philosophy']);
+    counts['politics'] += parseInt(person['politics']);
+    counts['military'] += parseInt(person['military']);
+    counts['religion'] += parseInt(person['religion']);
+  }
+  return counts;
 }
 
 function arraysIntersect(arrayOne, arrayTwo) {
@@ -140,6 +190,49 @@ function drawIndividualGraph(currentPlace) {
     drawGraph("#individual", "ind-svg", neighbors, individualEdgeArray);
 }
 
+function drawIndividualGraphAsCirclePack(currentPlace) {
+  var currentPlaceEdges = currentPlace['edges'];
+  if (currentPlaceEdges == null) return;
+
+  var neighbors = [];
+  for(var i = 0; i < currentPlaceEdges.length; i++) {
+      var edge = edgeArray[currentPlaceEdges[i]]; 
+      var target = edge.target;
+      var targetIndex = target['polis_number'] - 1;
+      neighbors.push({Toponym: placesdata[targetIndex]["Toponym"], 
+          value:edge['sharedPeople'].length});
+  }
+
+  var fill = d3.scale.category20c();
+
+  var bubble = d3.layout.pack()
+      .sort(null)
+      .size([width, height])
+      .padding(1.5);
+
+  var svg = d3.select("#individual").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "ind-svg");
+  var flattenedHeirarchy = {children:neighbors};
+  var node = svg.selectAll("g.node")
+      .data(bubble.nodes(flattenedHeirarchy)
+        .filter(function(d) { return !d.children; }))
+      .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("title")
+      .text(function(d) { return d['Toponym']; });
+
+  node.append("circle")
+      .attr("r", function(d) { 
+          return d.r ;    
+      })
+      .attr("fill", function(d) {
+          return "#FF0000";
+      });
+}
+
 // Draws graph with class 'graphClass' into specified div. Returns reference
 // to node object for further custom functionality.
 function drawGraph(divToAddTo, graphClass, nodes, edges) {
@@ -149,8 +242,8 @@ function drawGraph(divToAddTo, graphClass, nodes, edges) {
         .attr("class", graphClass);
 
     var force = d3.layout.force()
-        .charge(-250)
-        .linkDistance(100)
+        .charge(-300)
+        .linkDistance(70)
         .size([width, height]);
 
     force
@@ -171,14 +264,16 @@ function drawGraph(divToAddTo, graphClass, nodes, edges) {
         .enter().append("circle")
         .attr("class", "node")
         .attr("r", 5);
-
+    // Using bounding box example from:
+    // http://mbostock.github.com/d3/talk/20110921/bounding.html
     force.on("tick", function() {
-            link.attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
-            node.attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
+        node.attr("cx", function(d) { return d.x = Math.max(r, Math.min(width - r, d.x)); })
+          .attr("cy", function(d) { return d.y = Math.max(r, Math.min(height - r, d.y)); });
+
+        link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
         });
     return {nodes:node, edges:link}; 
 }
